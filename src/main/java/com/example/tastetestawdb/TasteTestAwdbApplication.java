96eddc75f15a38f6d4ce04c897d6f4d52b00b379
Mihai Popescu <mihai.popescu1109@gmail.com>
2026-04-05 11:16:13 +0300

    initial commit

---
+++ src/main/java/com/example/tastetestawdb/TasteTestAwdbApplication.java
@@ -0,0 +1,12 @@
+package com.example.tastetestawdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteTestAwdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasteTestAwdbApplication.class, args);
+    }
+}
